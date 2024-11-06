dedcf23c0a3a587eab0f16559f80077a2de1ea63
nortveeb2408 <conv.19aivfiiyfe2h7@fleep.io>
2024-11-06 19:55:01 +0200

    viimane kodune ülesanne

--- public/todo.js
+++ public/todo.js
@@ -1,6 +1,7 @@
 console.log('front end js töötab!!!')
 
 async function fetchTasks() {
+    console.log('fetching ...')
     const response = await fetch('/api/task')
 
     if (response.status !== 200) {
@@ -10,6 +11,46 @@ async function fetchTasks() {
 
     const todos = await response.json()
     console.log(todos)
+    showAllTasks(todos)
+}
+
+
+function showAllTasks(todos) {
+    //TODO - näita kõiki loetud taskisid
+    const ylesandedEL = document.getElementById('ylesanded')
+    console.log(ylesandedEL)
+    const headerHTML = getTasksHeader()
+    const rowsHTML = getTasksHTML(todos)
+    ylesandedEL.innerHTML = `
+    ${headerHTML}
+    ${rowsHTML}
+    `
+}
+
+function getTasksHeader() {
+    return `
+    <div class="tasks_header">
+        <div class="col_description" >Ülesanne</div>
+        <div class="col_priority">Tähtsus</div>
+    </div>
+    `
+}
+
+function getTasksHTML(tasks) {
+    let rowsHTML = ''
+    for (const task of tasks) {
+        rowsHTML += getOneTaskHTML(task)
+    }
+    return rowsHTML
+}
+
+function getOneTaskHTML(task) {
+    return `
+    <div class="tasks_row">
+        <div class="col_description" >${task.DESCRIPTION}</div>
+        <div class="col_priority">${task.PRIORITY}</div>
+    </div>
+    `
 }
 
 fetchTasks()
